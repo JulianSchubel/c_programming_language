181b65ec641c2bfafb32c8d3ed437e330418640c
Julian Schubel <schubeljulian@gmail.com>
2022-08-29 16:56:51 +0200

    2-5 any

--- chapter_2/exercises/2_5/any.h
+++
@@ -1,6 +0,0 @@
-#if !defined (ANY_H)
-#define ANY_H
-#include <string.h>
-int any(char * string, char * find_string);
-#endif
-
--- chapter_2/exercises/2_5/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "any.h"
-
-int main(int argc, char * argv[])
-{
-	printf("%d\n", any(argv[1], argv[2]));
-	return 0;
-}
