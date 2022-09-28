eb199116246da9a2ff3bfcaf4017faee24c1030d
Julian Schubel <schubeljulian@gmail.com>
2022-09-28 19:57:25 +0200

    set_bits.c

--- chapter_2/exercises/2-6_set_bits.c
+++ chapter_2/exercises/2-6_set_bits.c
@@ -1,4 +1,5 @@
 /* Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged */
+
 /* Bit position count p starts from 0 and at right end. Assume p and n are sensible positive values */
 
 #include <stdio.h>
