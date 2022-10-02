b4fe5b7aeb4eeb16f24a963e741ed960b314530c
Julian Schubel <schubeljulian@gmail.com>
2022-10-02 18:08:08 +0200

    reverse_polish_calculator.c

--- chapter_4/examples/reverse_polish_calculator.c
+++ chapter_4/examples/reverse_polish_calculator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
+#include <ctype.h>
 
 #define MAXOP   100 /* max size of operand or operator */
 #define NUMBER  '0' /* signal that a number was found */
@@ -34,7 +35,7 @@ int main(int argc, char * * argv)
             case '/':
                 /* '/' not commutative, hence need to enforce order */
                 op2 = pop();
-                if (op2 ! = 0.0) {
+                if(op2 != 0.0) {
                     push(pop() / op2);
                 }
                 else {
@@ -54,4 +55,70 @@ int main(int argc, char * * argv)
 
 #define MAXVAL  100 /* maximum depth of val stack */
 
-int sp = 0;
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+
+/* push: push f onto value pack */
+void push(double f)
+{
+    if(sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+    int i, c;
+    while((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if(!isdigit(c) && c != '.') {
+        return c;       /* not a number */ 
+    }
+    i = 0;
+    if(isdigit(c))      /* collect integer part */
+        while(isdigit(s[++i] = c = getch()))
+            ;
+    if(c == '.')        /* collect fractional part */
+        while(isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if(c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE     100 
+
+char buf[BUFSIZE];      /* buffer for ungetch() */
+int bufp = 0;           /* next free position in buf */
+
+int getch(void)         /* get a (possible pushed back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)     /* push character back on input */
+{
+    if(bufp >= BUFSIZE) 
+        printf("ungetch: too many characters\n");
+    else 
+        buf[bufp++] = c;
+}
+
