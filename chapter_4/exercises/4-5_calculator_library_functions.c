6776911059cb100bbf9deec9eb9e2117ece2173a
Julian Schubel <schubeljulian@gmail.com>
2022-10-03 07:55:37 +0200

    Added sin, exp, pow

--- chapter_4/exercises/4-5_calculator_library_functions.c
+++ chapter_4/exercises/4-5_calculator_library_functions.c
@@ -1,4 +1,4 @@
-/* Add commands to print the top element of the stack without popping, to duplicate it, and to swap the top two elements. Add a command to clear the stack. */
+/* Add access to library functions like sin, exp, and pow. */
 
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
