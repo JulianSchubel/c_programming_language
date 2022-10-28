39367d9c44f87f69250bbaf3bb04e0696160de4e
Julian Schubel <schubeljulian@gmail.com>
2022-10-28 21:07:08 +0200

    strcpy pointer v3

---
+++ chapter_5/examples/strcpy_pointer_v3.c
@@ -0,0 +1,6 @@
+void strcpy_pointer_v3(char *s, char *t) 
+{
+    /* '\0' has value 0, therefore comparison to '\0' unnecessary; very common expression in C programs */
+    while(*s++ = * t++)
+        ;
+}
