25d677cd92cb438e1091e63296ede2a8cd8d3215
Julian Schubel <schubeljulian@gmail.com>
2023-06-28 11:32:07 +0200

    qsort_v2.c

--- chapter_5/examples/qsort_v2.c
+++ chapter_5/examples/qsort_v2.c
@@ -3,8 +3,11 @@
 #include <string.h>
 
 #define MAXLINES 5000       /* max #lines to be sorted */
+#define MAXLEN 1000 /* max length of any input line */
 char *lineptr[MAXLINES];    /* pointers to text lines */
+char *alloc(int);
 
+int _getline(char *, int);
 int readlines( char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
@@ -17,6 +20,87 @@ int main(int argc, char *argv[])
     int nlines;             /* number of input lines read */
     int numeric = 0;        /* 1 if numeric sort */
 
+    if( argc > 1 && strcmp(argv[1], "-n") ==0 ) {
+        numeric = 1;
+    }
+    if( (nlines = readlines(lineptr, MAXLINES)) >= 0 ) {
+        _qsort( (void **) lineptr, 0, nlines-1, (int (*) (void*, void*)) (numeric ? numcmp : strcmp) );
+        writelines(lineptr, nlines);
+        return 0;
+    }
+    else {
+        printf("input too big to sirt\n");
+        return 1;
+    }
     return EXIT_SUCCESS;
 }
 
+/* readlines: read input lines) */
+int readlines(char *lineptr[], int maxlines) {
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while( (len = _getline(line, MAXLEN)) > 0 ) {
+        if( nlines >= maxlines || (p = alloc(len)) == NULL ) {
+            return -1;
+        }
+        else {
+            line[len-1] = '\0'; /* delete newline */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    }
+    return nlines;
+}
+
+/* writelines: write output lines */
+void writelines(char *lineptr[], int nlines) {
+    int i;
+    for( i = 0; i < nlines; ++i ) {
+        printf("%s\n", lineptr[i]);
+    }
+}
+
+/* getline: get line into s, return length */
+int _getline(char s[], int limit) {
+    int c, i;
+    i = 0;
+
+    while(--limit > 0 && (c = getchar()) != EOF && c != '\n') {
+        s[i++] = c;
+    }
+    if( c == '\n' ) {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* _qsort: sort v[left]...v[right] into inreasing order */
+void _qsort(void *v[], int left, int right, int (*comp)(void*, void*)) {
+    int i, last; 
+    void swap(void *v[], int, int);
+
+    if( left >= right) {  /* do nothing if array contains fewer than two elements */
+        return;
+    }
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for( i = left+1; i <= right; ++i ) {
+        if( (*comp)(v[i], v[left]) < 0 ) {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    _qsort(v, left, last-1, comp);
+    _qsort(v, last+1, right, comp);
+}
+
+void swap(void *v[], int i, int j)
+{
+    char *temp;
+    temp =  v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
